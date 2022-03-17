05e4becc08cd0fdd2c708978a078d2b58a8d7101
TakashiHK123 <takashihk@fiuna.edu.py>
2022-03-17 18:03:31 -0300

    entity

---
+++ src/main/java/com/example/Msc/Patients/entity/DatosPersonales.java
@@ -0,0 +1,14 @@
+package com.example.Msc.Patients.entity;
+
+import java.io.Serializable;
+
+public class DatosPersonales implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private int idDatosPersonales;
+    private String nombre;
+    private String apellido;
+    private String nroDocumento;
+    private String fechaNacimiento;
+    private double peso; 
+}
