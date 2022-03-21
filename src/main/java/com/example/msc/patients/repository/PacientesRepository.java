62769b9bea17c643926a1c487ad4cfef6b742d42
TakashiHK123 <takashihk@fiuna.edu.py>
2022-03-21 09:30:26 -0300

    prueba

--- src/main/java/com/example/msc/patients/repository/PacientesRepository.java
+++ src/main/java/com/example/msc/patients/repository/PacientesRepository.java
@@ -1,4 +1,11 @@
 package com.example.msc.patients.repository;
 
 public class PacientesRepository {
+
+    private static final String SQL="SELECT * FROM pacientes ";
+    private static final String SQL_POST = "INSERT INTO pacientes (id_datos_personales, fecha_ingreso, estado) VALUES (?, ?, ?)";
+    private static final String SQL_DELETE = "DELETE FROM pacientes WHERE id_paciente=?";
+    private static final String SQL_GET = "SELECT * FROM pacientes WHERE id_paciente = ?";
+
+
 }
